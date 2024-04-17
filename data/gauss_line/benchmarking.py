cb192c7af4d95d6b3b2639b04b91e4080e23f20d
Edith Heiter <edith.heiter@ugent.be>
2024-04-17 11:26:56 +0200

    benchmarking script

--- data/gauss_line/benchmarking.py
+++ data/gauss_line/benchmarking.py
@@ -43,37 +43,11 @@ def parse_args():
     return parser.parse_args()
 
 
-# this function is copied from Böhm et al. (2022)
-# https://github.com/berenslab/ne-spectrum/blob/56e7204710258d541fb716033d3542a4fca2705e/jnb_msc/generator/gauss_line.py#L72
-def gauss_clusters(
-    n_clusters=10,
-    dim=20,
-    pts_cluster=100,
-    random_state=None,
-    cov=1,
-    stepsize=6,
-):
-    if random_state is None:
-        rng = default_rng()
-    else:
-        rng = random_state
-
-    s = stepsize / np.sqrt(dim)
-    means = np.linspace(np.zeros(dim), n_clusters * s, num=n_clusters, endpoint=False)
-    cov = np.eye(dim) * cov
-
-    clusters = np.array(
-        [rng.multivariate_normal(m, cov, size=(pts_cluster)) for m in means]
-    )
-
-    X = np.reshape(clusters, (-1, dim))
-    y = np.repeat(np.arange(n_clusters), pts_cluster)
-    return X, y
-
 def random_data(n: int, d: int):
     rng = np.random.default_rng()
     data = rng.standard_normal(size=(n, d))
-    return data, np.zeros(n, dtype=int)
+    return data
+
 
 def second_to_str(sec):
     str_tm = str(datetime.timedelta(seconds=sec))
@@ -86,15 +60,11 @@ def second_to_str(sec):
     return f"{day}{hour} hour {minute} min {second} sec"
 
 
-def embedding_with_quality(size, d, compute_tsne=True):       
-    timings = {"n": int(size),
-                "d": int(d)}
+def embedding_with_quality(size, d, compute_tsne=True):
+    timings = {"n": int(size), "d": int(d)}
     size = int(size)
     print(f"Dataset size: n={size}, d={d}")
-    if size > 1000 and d > 1000:
-        data, labels = random_data(size, d)
-    else:
-        data, labels = gauss_clusters(pts_cluster=int(size / 10), dim=d)
+    data = random_data(size, d)
 
     start_pca = time.time()
     pca_emb = sc.pp.pca(data, n_comps=2, zero_center=True)
@@ -106,12 +76,12 @@ def embedding_with_quality(size, d, compute_tsne=True):
         sampling_frac = 1 if size < 100000 else 1 / (10 * (np.log10(size) - 3))
         tsne_embs = compute_tsne_series(
             data=data,
-            coarse_exag_iter=[(12, 200)],
+            coarse_exag_iter=[],
             fine_exag_iter=[(10, 200), (5, 200), (3, 200), (1, 200)],
             hd_metric="euclidean",
             init=pca_emb,
             sampling_frac=sampling_frac,
-            smoothing_perplexity=min(30, int(size/3)),
+            smoothing_perplexity=min(30, int(size / 3)),
             random_state=42,
             n_jobs=12,
         )
@@ -127,12 +97,11 @@ def embedding_with_quality(size, d, compute_tsne=True):
     start_trace = time.time()
     trace_data = TraceData(
         hd_data=data,
-        name=f"Gaussian Line {size}",
+        name=f"Random Bechmarking Data {size}",
         verbose=True,
         hd_metric="euclidean",
         n_jobs=12,
     )
-    trace_data.add_metadata({"labels": labels.astype(int)})
     trace_data.add_embedding(
         name="PCA",
         embedding=pca_emb,
@@ -141,43 +110,50 @@ def embedding_with_quality(size, d, compute_tsne=True):
 
     for i, emb in tsne_embs.items():
         if compute_tsne:
-            trace_data.add_embedding(
-                name=f"tSNE_{i}", embedding=emb, category="tSNE"
-            )
+            trace_data.add_embedding(name=f"tSNE_{i}", embedding=emb, category="tSNE")
         else:
             trace_data.add_embedding(
                 name=f"random_{i}", embedding=emb, category="random"
             )
     timings["trace_s"] = time.time() - start_trace
-    
+
     start_quality = time.time()
     trace_data.compute_quality()
     timings["quality_s"] = time.time() - start_quality
-    timings["quality_readable"] = second_to_str(
-        timings["quality_s"]
-    )
+    timings["quality_readable"] = second_to_str(timings["quality_s"])
     print("")
     return timings
-    
-    
+
+
 if __name__ == "__main__":
     print("Running Gaussian Line Benchmarking")
 
     args = parse_args()
     timings = pd.DataFrame(
-        columns=["n", "d", "pca_s", "tsne_s", "trace_s", "quality_s", "quality_readable"]
+        columns=[
+            "n",
+            "d",
+            "pca_s",
+            "tsne_s",
+            "trace_s",
+            "quality_s",
+            "quality_readable",
+        ]
     )
-    
+
     # numba warmup
     _ = embedding_with_quality(1e1, 20, compute_tsne=True)
 
     for size in args.n:
         for dim in args.d:
             result_dict = embedding_with_quality(size, d=dim, compute_tsne=args.tsne)
-            timings = pd.concat([timings if not timings.empty else None, pd.DataFrame([result_dict])], ignore_index=True)
-            
+            timings = pd.concat(
+                [timings if not timings.empty else None, pd.DataFrame([result_dict])],
+                ignore_index=True,
+            )
+
             timings.to_csv(
                 args.output,
                 float_format="%.2f",
                 index=False,
-                )
+            )
